6178a04c04047dc50f1f4ad81f79b82934fc1a4e
Daniel Santos De Sousa <daniel.s.6140@gmail.com>
2024-12-06 11:47:59 -0300

    feat(domain): Add LastUpdateAt property to BaseEntity

--- backend/StockChat/StockChat.Domain/Abstractions/BaseEntity.cs
+++ backend/StockChat/StockChat.Domain/Abstractions/BaseEntity.cs
@@ -4,6 +4,7 @@ public abstract class BaseEntity
 {
     public Guid Id { get; set; }
     public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset LastUpdateAt { get; set; }
     protected BaseEntity()
     {
         Id = Guid.NewGuid();
