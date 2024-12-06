f373eed81163b2b62d7fdb05c3f3aec86ea14e07
Daniel Santos De Sousa <daniel.s.6140@gmail.com>
2024-12-06 11:26:48 -0300

    feat(authentication): Add AuthenticationController with registration and login endpoints
    - Implement AuthenticationController to handle user registration and login requests.

---
+++ backend/StockChat/StockChat.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using StockChat.Domain.Contracts.Services;
+using StockChat.Domain.Entities;
+
+namespace StockChat.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthenticationController : ControllerBase
+{
+    public record LoginRequest(string Email, string Password);
+    public record RegisterRequest(string Email, string Password, string FullName);
+
+    private readonly IAuthenticationService _authService;
+
+    public AuthenticationController(IAuthenticationService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequest request)
+    {
+        var result = await _authService.LoginAsync(request.Email, request.Password);
+
+        if (!result.IsSuccess)
+            return BadRequest(result.Errors);
+        
+        return NoContent();
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+    {
+        var user = new User
+        {
+            Email = request.Email,
+            FullName = request.FullName,
+        };
+
+
+        var result = await _authService.RegisterAsync(user, request.Password);
+
+        if (!result.IsSuccess)
+            return BadRequest(result.Errors);
+        
+        return NoContent();
+    }
+
+    [HttpPost("logout")]
+    public async Task<IActionResult> Logout()
+    {
+        await _authService.LogoutAsync();
+        return NoContent();
+    }
+}
